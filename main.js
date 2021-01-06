346569ff17ae022c85ac93f7d893383048aeb58d
frogcat <frogcat@users.noreply.github.com>
2021-01-06 16:43:00 +0900

    Update テストをパスするように復旧、意図的なオートフォーマットの適用

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const jisx0401 = require("./lib/jisx0401.json");
 
-const resolve = function (code) {
+const resolve = function(code) {
   if (typeof code === "number") return resolve(code.toString());
   if (typeof code !== "string") return undefined;
   if (code.length === 1) return resolve("0" + code);
   return jisx0401[code];
 };
 
-const find = function (name) {
+const find = function(name) {
   if (!name) return undefined;
-  const lc = name.toUpperCase();
+  const lc = name.toLowerCase();
   return Object.keys(jisx0401).find((code) => {
     const values = Object.values(jisx0401[code]).map((a) => a.toLowerCase());
     return values.indexOf(lc) !== -1;
@@ -18,5 +18,5 @@ const find = function (name) {
 
 module.exports = {
   resolve: resolve,
-  find: find,
+  find: find
 };
