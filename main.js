f5eb082934b55932de5767793d29827894a25a79
frogcat <frogcat@users.noreply.github.com>
2021-01-06 16:14:46 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const jisx0401 = require("./lib/jisx0401.json");
+
+const resolve = function(code) {
+  if (typeof code === 'number') return resolve(code.toString());
+  if (typeof code !== 'string') return undefined;
+  if (code.length === 1) return resolve("0" + code);
+  return jisx0401[code];
+};
+
+const find = function(name) {
+  if (!name) return undefined;
+  const lc = name.toLowerCase();
+  return Object.keys(jisx0401).find(code => {
+    const values = Object.values(jisx0401[code]).map(a => a.toLowerCase());
+    return values.indexOf(lc) !== -1;
+  });
+};
+
+module.exports = {
+  resolve: resolve,
+  find: find
+};
